5017937b7921ccd322ec9e11185de6de0ae0452a
Lucas Barboza <lllucas.barboza@gmail.com>
2024-09-21 01:21:02 -0300

    update

---
+++ codigo_projeto.R
@@ -0,0 +1,39 @@
+if(!require(pacman)) {
+  install.packages("pacman")
+} else {
+  pacman::p_load("dplyr","DescTools","car","rstatix","ggplot2","qqplotr", "multcomp")
+}
+
+data(PlantGrowth)
+head(PlantGrowth)
+
+PlantGrowth %>% group_by(group) %>% get_summary_stats(weight)
+
+leveneTest(weight ~ group, PlantGrowth, center = median)
+
+boxplot(weight ~ group,data = PlantGrowth,
+        ylab = "", xlab = "", main = "Boxplot do Peso por Grupo")
+
+PlantGrowth[PlantGrowth$group == 'trt1', ]
+
+modelo <- lm(weight ~ group, data = PlantGrowth)
+resultado_anova <- anova(modelo)
+
+shapiro.test(residuals(modelo))
+
+PlantGrowth$residuos <- residuals(modelo)
+
+ggplot(PlantGrowth, aes(sample = residuos)) +
+  stat_qq_band() +
+  stat_qq_line() +
+  stat_qq_point() +
+  labs(title = "Gráfico Q-Q dos Resíduos", x = "Quantis Teóricos", y = "Quantis Amostrais") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+resultado_anova
+
+dunnett <- glht(modelo, linfct = mcp(group = "Dunnett"))
+summary(dunnett)
+
+plot(dunnett, main = "Intervalo de Confiança (95%) das Diferenças das Médias", 
+     xlab = "Diferença das Médias")
